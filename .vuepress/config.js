0e530f824433d1ea10fedc0724c2b1d1fb7898f4
exsoul <soul@Soul-MacBook-Pro.local>
2018-04-17 00:11:45 +0900

    試しに作成してみた

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'exSOUL.github.io',
+  description: 'exSOULのSandboxみたいなもの',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Twitter', link: 'https://twitter.com/ex_SOUL' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+  }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+---
+home: true
+---
+
+# exSOUL.github.io
+
+## VuePressをちょっと使ってみた
+このファイル自体はREADME.md  
+```
+vuepress build
+```
+
+を実行することでREADME.mdに書いたマークダウンがHTMLとして出力されるみたい。
+
+
